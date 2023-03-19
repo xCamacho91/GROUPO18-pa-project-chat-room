7d4f35df91328e93ff0fbbfacf26e14e48c36c4f
Maurício Camacho <joaocamacho91@gmail.com>
2023-03-19 21:28:17 +0000

    test edit the filter test

--- server/src/test/ServerTest.java
+++ server/src/test/ServerTest.java
@@ -1,19 +1,14 @@
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class ServerTest {
     @Test
-    public void TestServerLog() throws IOException {
-        ServerSocket listener = new ServerSocket(8080);
-        Socket client = listener.accept();
+    public void TestServerLog() {
         String message = "youre a dumbass";
-        ConnectionHandler server = new ConnectionHandler(client, new ArrayList<>(), 8080);
-        String filterMessage = server.filter("filtro.txt",message);
+        FilterMessage server = new FilterMessage(message);
+        String filterMessage = server.filter();
         assertEquals("youre a dumb****",filterMessage);
     }
 
